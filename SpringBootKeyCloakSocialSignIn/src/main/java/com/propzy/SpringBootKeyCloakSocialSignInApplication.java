df345ce25f612a0d5d162a32c6be1e50977ec4dd
loizenai <62205284+loizenai@users.noreply.github.com>
2021-12-15 16:44:42 +0700

    Add files via upload

---
+++ SpringBootKeyCloakSocialSignIn/src/main/java/com/propzy/SpringBootKeyCloakSocialSignInApplication.java
@@ -0,0 +1,13 @@
+package com.propzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeyCloakSocialSignInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKeyCloakSocialSignInApplication.class, args);
+	}
+
+}
